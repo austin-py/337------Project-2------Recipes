b4e8dcf02051fa903972b50ce20c8955285ed3ab
lfang1 <fredfang1203@gmail.com>
2022-06-01 22:22:04 -0500

    Update step_parser.py
    create get_directions function

--- src/step_parser.py
+++ src/step_parser.py
@@ -1,5 +1,6 @@
-from classes import Step
-
+from classes import *
+from read_json import load_recipe
+from read_list import get_kichen_utensils_list
 '''
 self.step_number = number
 self.ingredients = []
@@ -9,3 +10,18 @@ self.time = 0
 self.description = ''
 '''
 
+def get_directions(filename=None):
+    recipe_data = load_recipe(filename)
+    recipe_name = recipe_data['Recipe Name']
+    directions = recipe_data['directions']
+    recipe = Recipe(recipe_name)
+    recipe.tools = get_kichen_utensils_list()
+    print(recipe.tools)
+    return directions
+
+
+def main():
+    print(get_directions())
+
+if __name__=='__main__':
+    main()
