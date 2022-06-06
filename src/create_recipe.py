b257f5b2c28c58ed9aa6c915458074c9317cc904
austin-py <austinporras2023@u.northwestern.edu>
2022-06-06 12:21:16 -0500

    tools added

--- src/create_recipe.py
+++ src/create_recipe.py
@@ -2,14 +2,15 @@ from classes import *
 from soup import * 
 from ingredient_parser import * 
 from step_parser import get_directions as get_steps
+from step_parser import get_used_tools
 from method_parser import * 
 
 def create_recipe(url):
     rv = Recipe(url_to_name(url))
     data = get_soup_dictionary(url)
     rv.ingredients = get_ingredient_list(data)
-    # rv.tools = 
     rv.methods = parse_method(data)
     rv.steps = get_steps(data)
+    rv.tools = get_used_tools(rv.steps)
 
     return rv 
