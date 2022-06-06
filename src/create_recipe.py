b8d2ce7b01666a8461d3140be98ba4f8744297f9
austin-py <austinporras2023@u.northwestern.edu>
2022-06-06 11:46:22 -0500

    recipe maker

--- src/create_recipe.py
+++ src/create_recipe.py
@@ -1,4 +1,15 @@
-import classes 
+from classes import * 
+from soup import * 
+from ingredient_parser import * 
+from step_parser import * 
+from method_parser import * 
 
 def create_recipe(url):
-    
+    rv = Recipe(url_to_name(url))
+    data = get_soup_dictionary(url)
+    rv.ingredients = get_ingredient_list(data)
+    # rv.tools = 
+    rv.methods = parse_method(data)
+    rv.steps = get_directions(data)
+
+    return rv 
